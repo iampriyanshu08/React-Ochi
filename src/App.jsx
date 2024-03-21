02f16d11f7597f202071327b6ac9bb9391529381
Priyanshu Utsahi <utsahiji.priyanshu2000@gmail.com>
2024-03-21 14:52:38 +0530

    nav is completed for now

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <div className='h-screen w-full bg-zinc-900 text-white'>
+    <Navbar/>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
